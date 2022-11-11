7e9ea24b58c1d2459f236a1e4f65be50f9783ca9
ndehaven <106265519+ndehaven@users.noreply.github.com>
2022-11-10 19:12:30 -0700

    Update Chef.java

--- src/main/java/ChefM/Chef.java
+++ src/main/java/ChefM/Chef.java
@@ -2,12 +2,15 @@ package ChefM;
 import java.util.*;
 
 
+typedef enum orderStatus{ 
+    ORDER_STARTED, ORDER_COOKING, ORDER_READY
+  }orderStatus;
+
+
 public class Chef{ 
   
   
-  
-  
-  void changeStatus() {
+  void changeStatus(orderStatus order) {
     
     
   }
