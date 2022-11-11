8e6d70fff9a97713a61eab2c8ff846c2e159f727
AidanMcClean <aidanmccleanxd@gmail.com>
2022-11-10 20:40:38 -0700

    adding queue to processdisplay

--- src/main/java/ProcessorM/ProcessDisplay.java
+++ src/main/java/ProcessorM/ProcessDisplay.java
@@ -1,24 +1,30 @@
 package ProcessorM;
 
+import PizzaOrder.PizzaOrder;
+import CustomerM.Customer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.TextFieldTreeTableCell;
 import javafx.stage.Stage;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 
 public class ProcessDisplay {
 
+    private Queue<Customer> CustomerList = new LinkedList<>();
     @FXML
     private TextField tfTitle;
+    private Customer currentcustomer = new Customer();
 
 
     @FXML
     void acceptOrder(ActionEvent event) {
 
-
+        currentcustomer = CustomerList.remove();
 
     }
 
