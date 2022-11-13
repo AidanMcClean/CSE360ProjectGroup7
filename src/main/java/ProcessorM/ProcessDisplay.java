26d9d8cf0f100bf71f1b92454bbe718a63709f61
AidanMcClean <aidanmccleanxd@gmail.com>
2022-11-13 13:36:47 -0700

    Cleaning up classes
    got rid of Processor.java, Chef.java, and Customer.java

--- src/main/java/ProcessorM/ProcessDisplay.java
+++ src/main/java/ProcessorM/ProcessDisplay.java
@@ -1,25 +1,17 @@
 package ProcessorM;
 
 import PizzaOrder.PizzaOrder;
-import CustomerM.Customer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
-import javafx.scene.control.cell.TextFieldTreeTableCell;
-import javafx.stage.Stage;
 
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Stack;
 
 
 public class ProcessDisplay {
 
-<<<<<<< Updated upstream
-    public Queue<Customer> CustomerList = new LinkedList<>();
-=======
-    private Queue<PizzaOrder> CustomerList = new LinkedList<>();
->>>>>>> Stashed changes
+    public Queue<PizzaOrder> CustomerList = new LinkedList<>();
     @FXML
     private TextField tfTitle;
     private PizzaOrder currentcustomer = new PizzaOrder();
