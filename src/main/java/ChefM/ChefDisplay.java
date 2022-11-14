4bb143b76cf2f2a82fbfab995107c3ebda4dff68
ASU-Aaron <103948702+ASU-Aaron@users.noreply.github.com>
2022-11-13 20:20:12 -0700

    Update ChefDisplay.java
    changed asuID to PizzaNumber

--- src/main/java/ChefM/ChefDisplay.java
+++ src/main/java/ChefM/ChefDisplay.java
@@ -1,25 +1,36 @@
 package ChefM;
 
+import database.OrderConnection;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import jdk.jfr.TransitionFrom;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.Stack;
-//this is a test
+import java.sql.*;
+import static database.OrderConnection.getConnection;
+
 
 public class ChefDisplay {  
 
     @FXML
     void cookingStatus(ActionEvent event) {
-        
+        //1 in database
+        OrderConnection.updateStatus("Cooking", 1);
     }
 
     @FXML
     void finishStatus(ActionEvent event) {
-
+    //2 in database
     }
 
     @FXML
     void readyStatus(ActionEvent event) {
-
+    //0 in database
     }
+
+
+
 
 }
