a4e7348426f609bc05db1130d261951417d364d4
ndehaven <106265519+ndehaven@users.noreply.github.com>
2022-12-01 23:16:38 -0700

    TestCases updated

--- src/main/java/TestCases.java
+++ src/main/java/TestCases.java
@@ -7,23 +7,12 @@ import org.junit.*;
 import static org.junit.Assert.*;
 import static org.junit.Test.*;
 
-
-
 public class TestCases {
 
     String asuID = "email";
     String asuEmail = asuID + "@asu.edu";
     String cookStatus = " ";
     int PizzaNumber = 0;
-    int pizzaType = 0; //0: pepperoni, 1: veggie, 2: cheese
-    boolean mushroom = false;
-    boolean onion = false;
-    boolean olives = false;
-    boolean extraCheese = false;
-    String pickupTime = "0:00";
-    boolean acceptedStatus = false;
-
-
     PizzaOrder pizzaTester = new PizzaOrder(1, "ndehaven", 1, true, true, true,
             true, "20:00", true, "Ready");
     @Test
@@ -32,14 +21,12 @@ public class TestCases {
                 0,
                 pizzaTester.getPizzaNumber());
     }
-
     @Test
     public void displayStatusTest() {
         assertEquals("displayStatus is not functioning correctly",
                 "",
                 pizzaTester.displayStatus());
     }
-
     @Test
     public void setStatusTest() {
         pizzaTester.setStatus("Cooking");
@@ -47,22 +34,16 @@ public class TestCases {
                 "Ready",
                 pizzaTester.getStatus());
     }
-
-
     @Test
     public void getIDTest(){
         assertEquals("getId is not functioning correctly",
                 "",
                 pizzaTester.getID());
     }
-
     @Test
     public void getEmailTest() {
         assertEquals("getEmail is not functioning correctly",
                 "email@asu.edu",
                 pizzaTester.getEmail());
     }
-
-
-
 }
